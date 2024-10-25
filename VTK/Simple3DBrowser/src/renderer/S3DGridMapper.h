f708f2435c21ca511826173f8ba032dfb17216fe
BeyondXin <779354187@qq.com>
2024-10-25 22:45:28 +0800

    [feat] 增加 Simple3DBrowser

---
+++ VTK/Simple3DBrowser/src/renderer/S3DGridMapper.h
@@ -0,0 +1,38 @@
+﻿#ifndef S3DGridMapper_h
+#define S3DGridMapper_h
+
+#include <vtkOpenGLPolyDataMapper.h>
+#include <vtkSmartPointer.h>
+
+class S3DGridMapper : public vtkOpenGLPolyDataMapper
+{
+public:
+    static S3DGridMapper * New();
+    vtkTypeMacro(S3DGridMapper, vtkOpenGLPolyDataMapper);
+
+    vtkSetMacro(FadeDistance, double);
+    vtkGetMacro(FadeDistance, double);
+
+    vtkSetMacro(UnitSquare, double);
+    vtkGetMacro(UnitSquare, double);
+
+    double * GetBounds() override;
+
+private:
+    S3DGridMapper();
+    ~S3DGridMapper() = default;
+    S3DGridMapper(const S3DGridMapper &) = delete;
+    void operator=(const S3DGridMapper &) = delete;
+
+    void ReplaceShaderValues(std::map<vtkShader::Type, vtkShader *> shaders, vtkRenderer * ren, vtkActor * actor) override;
+    void SetMapperShaderParameters(vtkOpenGLHelper & cellBO, vtkRenderer * ren, vtkActor * actor) override;
+    void BuildBufferObjects(vtkRenderer * ren, vtkActor * act) override;
+    void RenderPiece(vtkRenderer * ren, vtkActor * actor) override;
+    bool GetNeedToRebuildShaders(vtkOpenGLHelper & cellBO, vtkRenderer * ren, vtkActor * act) override;
+
+private:
+    double FadeDistance = 10.0; // ½¥Òþ¾àÀë
+    double UnitSquare = 1.0; // ±ê×¼»¯×ø±ê
+};
+
+#endif
---
+++ VTK/Simple3DBrowser/src/renderer/S3DRenderPass.h
@@ -0,0 +1,70 @@
+﻿#ifndef S3DRenderPass_h
+#define S3DRenderPass_h
+
+#include <vtkFramebufferPass.h>
+#include <vtkOpenGLQuadHelper.h>
+#include <vtkSmartPointer.h>
+#include <vtkTimeStamp.h>
+
+#include <memory>
+#include <vector>
+
+class vtkProp;
+
+class S3DRenderPass : public vtkRenderPass
+{
+public:
+    static S3DRenderPass * New();
+    vtkTypeMacro(S3DRenderPass, vtkRenderPass);
+
+    vtkGetMacro(UseRaytracing, bool);
+    vtkSetMacro(UseRaytracing, bool);
+    vtkBooleanMacro(UseRaytracing, bool);
+
+    vtkGetMacro(UseSSAOPass, bool);
+    vtkSetMacro(UseSSAOPass, bool);
+    vtkBooleanMacro(UseSSAOPass, bool);
+
+    vtkGetMacro(UseDepthPeelingPass, bool);
+    vtkSetMacro(UseDepthPeelingPass, bool);
+    vtkBooleanMacro(UseDepthPeelingPass, bool);
+
+    vtkGetMacro(UseBlurBackground, bool);
+    vtkSetMacro(UseBlurBackground, bool);
+    vtkBooleanMacro(UseBlurBackground, bool);
+
+    vtkGetVector6Macro(Bounds, double);
+    vtkSetVector6Macro(Bounds, double);
+
+    void Render(const vtkRenderState * s) override;
+
+private:
+    S3DRenderPass() = default;
+    ~S3DRenderPass() override = default;
+    S3DRenderPass(const S3DRenderPass &) = delete;
+    void operator=(const S3DRenderPass &) = delete;
+
+    void ReleaseGraphicsResources(vtkWindow * w) override;
+    void Initialize(const vtkRenderState * s);
+    void Blend(const vtkRenderState * s);
+
+private:
+    bool UseRaytracing = false;
+    bool UseSSAOPass = false;
+    bool UseDepthPeelingPass = false;
+    bool UseBlurBackground = true;
+
+    vtkSmartPointer<vtkFramebufferPass> BackgroundPass;
+    vtkSmartPointer<vtkFramebufferPass> MainPass;
+
+    double Bounds[6];
+
+    vtkMTimeType InitializeTime = 0;
+
+    std::vector<vtkProp *> BackgroundProps;
+    std::vector<vtkProp *> MainProps;
+
+    std::shared_ptr<vtkOpenGLQuadHelper> BlendQuadHelper;
+};
+
+#endif
