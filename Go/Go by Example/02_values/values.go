27d36a908a97fb02ae731dcbc3000426ea202c9c
BeyondXin <779354187@qq.com>
2024-11-23 16:08:33 +0800

    backup

---
+++ Go/Go by Example/02_values/values.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	fmt.Println("go" + "lang")
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7/2 =", 7/2)
+	fmt.Println("7.0/2.0 =", 7.0/2.0)
+
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+}
