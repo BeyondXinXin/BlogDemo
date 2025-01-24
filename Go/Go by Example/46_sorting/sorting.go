1838e1a707c1b51f1a0862224ed1db1330fafc79
BeyondXin <779354187@qq.com>
2025-01-24 16:48:43 +0800

    2025-01-24 周五 16:48

---
+++ Go/Go by Example/46_sorting/sorting.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+	strs := []string{"c", "a", "b"}
+
+	slices.Sort(strs)
+	fmt.Println("Strings:", strs)
+
+	ints := []int{7, 2, 4}
+	s := slices.IsSorted(ints)
+	fmt.Println("Sorted:", s)
+
+	slices.Sort(ints)
+	fmt.Println("Ints:", ints)
+
+	s = slices.IsSorted(ints)
+	fmt.Println("Sorted:", s)
+
+}
