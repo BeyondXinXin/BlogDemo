8e7d1706ae8490ff07807b39855d5a69f58b6a7d
BeyondXin <779354187@qq.com>
2025-02-06 10:15:44 +0800

    2025-02-06 Thursday 10:15

--- push.sh
+++ push.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-# 添加所有文件
-git add .
+# 设置中文环境获取星期
+LC_TIME=zh_CN.UTF-8
 
-# 获取当前日期和星期
+# 添加文件并提交
+git add .
 mydate=$(date +"%Y-%m-%d")
 weekday=$(date +"%A")
 mytime=$(date +"%H:%M")
-
-# 提交更改
 git commit -m "$mydate $weekday $mytime"
 
 # 推送到远程仓库
 git push
 
-# 保持终端窗口打开
+# 暂停
 read -p "Press any key to continue..." -n1 -s
-
