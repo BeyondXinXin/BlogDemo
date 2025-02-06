dcb3386c91288822584a569425739e1faa2947da
BeyondXin <779354187@qq.com>
2025-02-06 10:19:26 +0800

    2025-02-06 星期四 10:19

--- push.sh
+++ push.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-# 设置中文环境获取星期
-LC_TIME=zh_CN.UTF-8
-
 # 添加文件并提交
 git add .
 mydate=$(date +"%Y-%m-%d")
-weekday=$(date +"%A")
 mytime=$(date +"%H:%M")
+
+# 获取英文星期并映射到中文
+weekday=$(date +"%A")
+case $weekday in
+    "Monday") weekday="星期一" ;;
+    "Tuesday") weekday="星期二" ;;
+    "Wednesday") weekday="星期三" ;;
+    "Thursday") weekday="星期四" ;;
+    "Friday") weekday="星期五" ;;
+    "Saturday") weekday="星期六" ;;
+    "Sunday") weekday="星期天" ;;
+esac
+
+# 提交更改
 git commit -m "$mydate $weekday $mytime"
 
 # 推送到远程仓库
